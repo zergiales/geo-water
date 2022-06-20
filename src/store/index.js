7596f62e80e8c9843cef5c1ef233432679938f21
sergio sanchez lopez <zergiosanchezlopez@gmail.com>
2022-06-21 00:17:31 +0200

    store

--- src/store/index.js
+++ src/store/index.js
@@ -7,12 +7,21 @@ Vue.use(Vuex);
 export default new Vuex.Store({
   state: {
     msg: 'nombre de usuario',
+    user: {
+
+    },
   },
   getters: {
   },
   mutations: {
+    CargarLogin(state, obj) {
+      this.state.user = obj;
+    },
   },
   actions: {
+    actLogin({ commit }, obj) {
+      commit('mutLogin', obj);
+    },
   },
   modules: {
   },
